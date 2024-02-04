236eb063ebcb83ff80b40f26fe03639b11437005
Henriddiaz <henryddiaz696@gmail.com>
2024-02-03 19:07:26 -0600

    app with executable
    The app now has an executable file, with it is not necessary to have libreries or Python installed

--- pythonnewproject/main.py
+++ pythonnewproject/main.py
@@ -7,32 +7,32 @@ def obvio():
     root.destroy() 
     
 def Button_hover(event):
-    X = random.randint(10,700)
+    X = random.randint(10,800)
     Y = random.randint(10,400)
     my_button2.place(x=X, y=Y)
     
 def exit_(event):
-    X = random.randint(10,700)
+    X = random.randint(10,800)
     Y = random.randint(10,400)
     my_button2.place(x=X, y=Y)
     
 def on_closing():
-    # Esta función se llama cuando se intenta cerrar la ventana
-    messagebox.showinfo(message="¡SABIA QUE LO INTENTARIAS, CONTESTA LA PREGUNTA POR FAVOR :c !", title="")
+    #messagebox.showinfo(message="¡SABIA QUE LO INTENTARIAS, CONTESTA LA PREGUNTA POR FAVOR :c !", title="")
+    pass
 
 root = Tk ()
-root.geometry("790x500")
+root.geometry("900x500")
 root.configure(background='#F4D03F')
 root.title('RESPONDEME')
 
-label_0 = Label(root, text= "quieres ser mi amigo?", bg = '#F4D03F', fg = 'black', width=0, font=("Comic Sans MS", 40))
+label_0 = Label(root, text= "¿QUIERES SER MI AMIKO?", bg = '#F4D03F', fg = 'black', width=0, font=("Comic Sans MS", 40))
 label_0.place(x=100, y=70)
 
-my_button1 = Button(root, text="CHI UwU", width=7, height=1, font=("Comic Sans MS", 30), bg = '#FF4141', fg='white', command=obvio)
-my_button1.place(x=150, y=220)
+my_button1 = Button(root, text="CHI UꞷU", width=7, height=1, font=("Comic Sans MS", 30), bg = '#FF4141', fg='white', command=obvio)
+my_button1.place(x=200, y=220)
 
-my_button2 = Button(root, text="ÑO UnU", width=7, height=1, font=("Comic Sans MS", 30), bg = '#FF4141', fg='white')
-my_button2.place(x=450, y=220)
+my_button2 = Button(root, text="ÑO UNU", width=7, height=1, font=("Comic Sans MS", 30), bg = '#FF4141', fg='white')
+my_button2.place(x=550, y=220)
 
 my_button2.bind("<Enter>", Button_hover)
 my_button2.bind("<Leave>", exit_)
